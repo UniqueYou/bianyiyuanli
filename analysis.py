2f72886a6928cc5c3154a4357d2e9219f5a81597
Wang Song <59224055+qq415582152@users.noreply.github.com>
2022-03-13 20:53:09 +0800

    注释识别有点问题

--- analysis.py
+++ analysis.py
@@ -1,8 +1,9 @@
 """
 词法分析-单词识别
 """
-from main import get_code
 import pandas
+
+from main import get_code
 
 
 # 判断字符是否为字母
@@ -19,6 +20,7 @@ def is_digit(ch):
     return False
 
 
+# 识别标识符/关键字
 def get_identifier(string: str, index: int):
     status = 0
     idf = ''
@@ -43,27 +45,100 @@ def get_identifier(string: str, index: int):
         index += 1
 
 
+# 识别整数
 def get_number(string, index):
     status = 0
     number = ''
     while index < len(string):
         if status == 0:
             if string[index] == '0':
-                number += '0'
-                status = 2
+                status = 3
+                number += string[index]
             elif '1' <= string[index] <= '9':
                 status = 1
                 number += string[index]
         elif status == 1:
             if is_digit(string[index]):
+                status = 1
                 number += string[index]
             else:
                 status = 2
+        elif status == 2:
+            return number
+        elif status == 3:
+            if '0' <= string[index] <= '7':
+                status = 3
+                number += string[index]
+            elif string[index] == 'x' or string[index] == 'X':
+                number += string[index]
+                status = 5
+            else:
+                status = 4
+        elif status == 4:
+            return number
+
+        elif status == 5:
+            if is_digit(string[index]) or 'a' <= string[index] <= 'f' or 'A' <= string[index] <= 'F':
+                status = 6
+                number += string[index]
+        elif status == 6:
+            if is_digit(string[index]) or 'a' <= string[index] <= 'f' or 'A' <= string[index] <= 'F':
+                status = 6
+                number += string[index]
+            else:
+                status = 7
         else:
-            if number != '':
-                return number
-            status = 0
+            return number
         index += 1
+
+
+# 识别注释
+def get_notes(string: str, i: int):
+    status = 0
+    notes = ''
+    while i < len(string):
+        if status == 0:
+            if string[i] == '/':
+                status = 1
+                notes += string[i]
+            else:
+                status = 5
+        elif status == 1:
+            if string[i] == '/':
+                status = 2
+                notes += string[i]
+            elif string[i] == '*':
+                status = 3
+                notes += string[i]
+        elif status == 2:
+            if string[i] == '\n':
+                status = 5
+                notes += string[i]
+            else:
+                status = 2
+                notes += string[i]
+        elif status == 3:
+            if string[i] == '*':
+                status = 4
+                notes += string[i]
+            else:
+                status = 3
+                notes += string[i]
+        elif status == 4:
+            if string[i] == '/':
+                status = 5
+                notes += string[i]
+            else:
+                status = 3
+                notes += string[i]
+        else:
+            return notes
+        i += 1
+
+
+# 错误识别
+def get_mistake():
+    pass
 
 
 def main():
@@ -85,6 +160,11 @@ def main():
                 temp = get_number(code, i)
                 i += len(temp)
                 print('整数', (temp, key.get('整数')[0]))
+                continue
+            elif code[i] == '/':
+                temp = get_notes(code, i)
+                i += len(temp)
+                print("注释", temp)
                 continue
             else:
                 if key.get(code[i]) is not None:
--- demo.cpp
+++ demo.cpp
@@ -1,7 +1,13 @@
-#include <stdio.h>
+
 int main(){
-    int x = 1;
-    int y = 200;
-    printf("hello wolrd\n");
+    int x = 1;//注释一
+    int y = 200;//注释二
+    int z = 0x05;
+    /*注释三*/
+    /*
+        注释四  what can i do for you
+
+    *********/
+    int k = 0734;
     return 0;
 }
